8563e91d87a7024302591cfefcb3989b6daa1bf1
Michal Jamry <jamrymi@gmail.com>
2023-05-24 14:06:03 +0200

    initial

---
+++ www/next.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  transpilePackages: [
+    "@fullcalendar/core",
+    "@babel/preset-react",
+    "@fullcalendar/common",
+    "@fullcalendar/daygrid",
+    "@fullcalendar/interaction",
+    "@fullcalendar/react",
+  ],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "removeViewBox",
+                  active: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
