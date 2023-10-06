db77dbdfd569b7d0ffa73d908f48e1ebec69b288
Cem Akgul <cem.akgul@digimans.ai>
2023-10-06 10:53:06 +0000

    Small fix and adding a TODO

--- data/preprocess/shapes/generate_renderings.py
+++ data/preprocess/shapes/generate_renderings.py
@@ -13,12 +13,17 @@ BLENDER_RENDER_SCRIPT_PATH = os.path.join(os.path.dirname(__file__), 'generate_r
 FILE_FORMAT = "obj"
 
 shapes = glob(ROOT_SHAPES_PATH + f"*.{FILE_FORMAT}")
+print(f"Number of shapes found is : {len(shapes)}")
 
+## TODO [Cem] : Find a way to run Blender inside Docker containers.
+# Possible issues with OpenGL installation inside Docker container and memory issues to run into.
+# Possible design change to run blender on shapes beforehand to the retexturing pipeline.
+print(f"Path for Blender exec exists : {os.path.exists(BLENDER_EXEC_PATH)}")
 for shape in shapes:
     os.system(f"{BLENDER_EXEC_PATH}/blender -b -P\
                 {BLENDER_RENDER_SCRIPT_PATH}\
                 --shape {Path(shape)}\
-                --root {ROOT_SHAPES_PATH}\
+                --root {Path(ROOT_SHAPES_PATH)}\
                 --focal_legth {54}\
                 --cam_args {[90, 0]}\
                 --sample_count {1024}\
